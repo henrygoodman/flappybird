a66e6ce4b756c7b81c2ad33418f4ff18b40a0583
Henry Goodman <13032204@student.uts.edu.au>
2021-05-28 19:29:23 +1000

    asdf

---
+++ flappybird.py
@@ -0,0 +1,22 @@
+import numpy
+import pygame
+import random
+
+width = 600
+height = 400
+size = width, height
+screen = pygame.display.set_mode(size)
+pygame.init()
+game_over = False
+
+class Bird():
+    def __init__(self):
+        self.x = width/2
+        self.y = height/2
+    
+class Bar():
+    def __init__(self):
+        self.x = width
+        se
+while not game_over:
+    pass
